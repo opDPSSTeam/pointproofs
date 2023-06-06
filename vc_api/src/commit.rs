73cb74491254e99f8426f6727d47bd30bd3de87a
Yanpei Guo <2847399255@qq.com>
2023-06-06 14:38:47 +0800

    cargo fmt && cargo fix

--- vc_api/src/commit.rs
+++ vc_api/src/commit.rs
@@ -1,13 +1,13 @@
+use super::util;
 use ark_bls12_381::Bls12_381;
-use ark_bls12_381::Fr;
-use ark_ec::PairingEngine;
+
+
+
 use pointproof::Commitment;
 use pointproof::CommitmentScheme;
 use pointproof::ProverParam;
 use pointproof::StructuredReferenceString;
 use pointproof::VerifierParam;
-use ark_std::rand::Rng;
-use super::util;
 
 pub fn gen_params<const N: usize>() -> String {
     let mut rng = rand::thread_rng();
@@ -15,10 +15,7 @@ pub fn gen_params<const N: usize>() -> String {
     util::srs_to_str(srs)
 }
 
-pub fn commit<const N: usize>(
-    srs: &str,
-    messages: &str,
-) -> String {
+pub fn commit<const N: usize>(srs: &str, messages: &str) -> String {
     let srs = util::str_to_srs(srs);
     let prover_param: ProverParam<Bls12_381, N> = (&srs).into();
     let messages = util::str_to_messages(messages);
@@ -28,11 +25,7 @@ pub fn commit<const N: usize>(
     util::g1_to_str(commitment)
 }
 
-pub fn open<const N: usize>(
-    srs: &str,
-    messages: &str,
-    pos: usize,
-) -> String {
+pub fn open<const N: usize>(srs: &str, messages: &str, pos: usize) -> String {
     let srs = util::str_to_srs(srs);
     let prover_param: ProverParam<Bls12_381, N> = (&srs).into();
     let messages = util::str_to_messages(messages);
--- vc_api/src/lib.rs
+++ vc_api/src/lib.rs
@@ -1,47 +1,27 @@
-mod util;
 mod commit;
+mod util;
 
 extern crate libc;
 use std::ffi::{CStr, CString};
 
 #[no_mangle]
-pub extern "C" fn fr_plus(
-    s1: *const libc::c_char,
-    s2: *const libc::c_char
-) -> *const libc::c_char {
-    let s1_str = unsafe {
-        CStr::from_ptr(s1).to_str().unwrap()
-    };
-    let s2_str = unsafe {
-        CStr::from_ptr(s2).to_str().unwrap()
-    };
+pub extern "C" fn fr_plus(s1: *const libc::c_char, s2: *const libc::c_char) -> *const libc::c_char {
+    let s1_str = unsafe { CStr::from_ptr(s1).to_str().unwrap() };
+    let s2_str = unsafe { CStr::from_ptr(s2).to_str().unwrap() };
     let f1 = util::str_to_message(s1_str);
     let f2 = util::str_to_message(s2_str);
     let res = util::message_to_str(f1 + f2);
     CString::new(res).unwrap().into_raw()
 }
 
-const GENERATE_PARAMS: [fn() -> String; 2] = [
-    commit::gen_params::<4>,
-    commit::gen_params::<7>,
-];
-const COMMIT: [fn(&str, &str) -> String; 2] = [
-    commit::commit::<4>,
-    commit::commit::<7>,
-];
-const OPEN: [fn(&str, &str, usize) -> String; 2] = [
-    commit::open::<4>,
-    commit::open::<7>,
-];
-const VERIFY: [fn(&str, &str, &str, usize, &str) -> bool; 2] = [
-    commit::verify::<4>,
-    commit::verify::<7>,
-];
+const GENERATE_PARAMS: [fn() -> String; 2] = [commit::gen_params::<4>, commit::gen_params::<7>];
+const COMMIT: [fn(&str, &str) -> String; 2] = [commit::commit::<4>, commit::commit::<7>];
+const OPEN: [fn(&str, &str, usize) -> String; 2] = [commit::open::<4>, commit::open::<7>];
+const VERIFY: [fn(&str, &str, &str, usize, &str) -> bool; 2] =
+    [commit::verify::<4>, commit::verify::<7>];
 
 #[no_mangle]
-pub extern "C" fn generate_params(
-    number: libc::c_int
-) -> *const libc::c_char {
+pub extern "C" fn generate_params(number: libc::c_int) -> *const libc::c_char {
     let res = GENERATE_PARAMS[number as usize]();
     CString::new(res).unwrap().into_raw()
 }
@@ -52,12 +32,8 @@ pub extern "C" fn commit(
     srs: *const libc::c_char,
     messages: *const libc::c_char,
 ) -> *const libc::c_char {
-    let srs = unsafe {
-        CStr::from_ptr(srs).to_str().unwrap()
-    };
-    let messages = unsafe {
-        CStr::from_ptr(messages).to_str().unwrap()
-    };
+    let srs = unsafe { CStr::from_ptr(srs).to_str().unwrap() };
+    let messages = unsafe { CStr::from_ptr(messages).to_str().unwrap() };
     let res = COMMIT[number as usize](srs, messages);
     CString::new(res).unwrap().into_raw()
 }
@@ -67,14 +43,10 @@ pub extern "C" fn open(
     number: libc::c_int,
     srs: *const libc::c_char,
     messages: *const libc::c_char,
-    pos: libc::c_int
+    pos: libc::c_int,
 ) -> *const libc::c_char {
-    let srs = unsafe {
-        CStr::from_ptr(srs).to_str().unwrap()
-    };
-    let messages = unsafe {
-        CStr::from_ptr(messages).to_str().unwrap()
-    };
+    let srs = unsafe { CStr::from_ptr(srs).to_str().unwrap() };
+    let messages = unsafe { CStr::from_ptr(messages).to_str().unwrap() };
     let res = OPEN[number as usize](srs, messages, pos as usize);
     CString::new(res).unwrap().into_raw()
 }
@@ -86,20 +58,12 @@ pub extern "C" fn verify(
     commitment: *const libc::c_char,
     message: *const libc::c_char,
     pos: libc::c_int,
-    witness: *const libc::c_char
+    witness: *const libc::c_char,
 ) -> libc::c_int {
-    let srs = unsafe {
-        CStr::from_ptr(srs).to_str().unwrap()
-    };
-    let commitment = unsafe {
-        CStr::from_ptr(commitment).to_str().unwrap()
-    };
-    let message = unsafe {
-        CStr::from_ptr(message).to_str().unwrap()
-    };
-    let witness = unsafe {
-        CStr::from_ptr(witness).to_str().unwrap()
-    };
+    let srs = unsafe { CStr::from_ptr(srs).to_str().unwrap() };
+    let commitment = unsafe { CStr::from_ptr(commitment).to_str().unwrap() };
+    let message = unsafe { CStr::from_ptr(message).to_str().unwrap() };
+    let witness = unsafe { CStr::from_ptr(witness).to_str().unwrap() };
 
     let res = VERIFY[number as usize](srs, commitment, message, pos as usize, witness);
     res as libc::c_int
